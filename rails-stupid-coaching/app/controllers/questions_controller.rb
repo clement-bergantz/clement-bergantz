4d22d2649f4b8cb8ea55092caa156f1fd2e6e8a7
clement bergantz <bergantz.clement@gmail.com>
2017-08-07 15:44:42 +0200

    finished exercice 1

---
+++ rails-stupid-coaching/app/controllers/questions_controller.rb
@@ -0,0 +1,19 @@
+class QuestionsController < ApplicationController
+  def answer
+    @input = params[:question]
+    @answer = coach_answer(@input)
+  end
+
+  def coach_answer(your_message)
+    if your_message.downcase == "i am going to work right now!"
+      ""
+    elsif your_message.end_with?("?")
+      "Silly question, get dressed and go to work!"
+    else
+      "I don't care, get dressed and go to work!"
+    end
+  end
+
+  def ask
+  end
+end
